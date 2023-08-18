3e997ad5aa1be42ac6ba602d6b9a49cc6178c22d
Yujin Yano <northwindapps@gmail.com>
2023-08-18 13:42:55 +0900

    testing in calculatorjs

--- calculator.js
+++ calculator.js
@@ -2,19 +2,110 @@ const {Service} = require('./Service');
 const Decimal = require('decimal.js');
 const service = new Service();
 
-function exec() {
+function exec(str) {
 console.log(Math.PI);
 
 const test = new Decimal(0.1);
 console.log(test);
-let str = '0.1 ^ 0.2 + sin60';
 str = service.scientific_operation(str);
 console.log(service.basic_operation(str));
 
+str = service.scientific_operation(str);
+console.log(service.basic_operation(str));
+
+
 }
 
+let str = '0.1 ^ 0.2 + sin60';
+str = 'sqrt(4) + (sin60^2 + cos60^2)';
+// str = 'sqrt(2)';
+let result = excecute(str);
+console.log(result);
 
-exec();
+function excecute(expression) {
+    let tempStr = null;
+    const charset = new Set("ABCDEFGHIJKLMNOPQRSTUVWXYZ");
+    if ([...expression].some(char => charset.has(char))) {
+        return false;
+    } else {
+        tempStr = expression.replace(/=/g, "");
+        // Continue using tempStr or perform other operations
+    }
+
+    //screening
+    let firstChar = tempStr[0];
+    if (firstChar === "^"){        
+        return false;
+    }else if (firstChar === "/"){
+        return false;
+    }else if (firstChar === "*"){
+        return false;
+    }else if (firstChar === "-"){
+        return false;
+    }else if (firstChar === "+"){
+        return false;
+    }
+    
+    var elements = new Array();
+    var bz_local = 0;
+    var startindex = -1; 
+    const MAXIMUM_LOOP_NUM = 50;
+    let loopcounter = MAXIMUM_LOOP_NUM;
+    
+    //PREPARATION
+    tempStr = service.replace_constant(tempStr);
+    
+    //Comma Free
+    tempStr = tempStr.replaceAll(",", "");    
+    if(!tempStr.includes("(")){
+        loopcounter = 0;
+        //no braces
+        if (!service.isFloat(tempStr)){
+            tempStr = service.scientific_operation(tempStr);
+            tempStr = service.basic_operation(tempStr);
+        }
+    }
+    
+    while (loopcounter > 0)  {
+        tempStr = tempStr.replaceAll(' ','');
+        let matches = tempStr.match(/\(([^)]+)\)/g);
+        if (matches) {
+            let cloned = Array.from(matches);
+            //(())の処理を行う    
+            let result = null;
+            for (let i = 0; i < cloned.length; i++){
+                if(service.containsAlphabetChars(cloned[i])){
+
+                    result = service.scientific_operation(cloned[i].replace('(','').replace(')',''));
+                    if (result) {
+                        tempStr = tempStr.replace(cloned[i],result);
+                    }        
+                }else{
+                    result = service.basic_operation(cloned[i].replace('(','').replace(')',''));
+                    if (result) {
+                        let ptn = cloned[i];
+                        tempStr = tempStr.replace(ptn,result);
+                    }           
+                }
+            }
+        }
+
+        tempStr = service.scientific_operation(tempStr);
+        tempStr = service.basic_operation(tempStr);   
+        
+        if (service.isFloat(tempStr)){
+            loopcounter = 0;
+        }
+        loopcounter -= 1;
+    }
+    
+    if (service.isFloat(tempStr)){
+        return tempStr;
+    }else{
+        return false;
+    }
+}
+
 
 // class Calculator{ 
 // }
--- service.js
+++ service.js
@@ -204,231 +204,243 @@ class Service {
     // }
     
     
+    /*
+    const input = "sqrt4+sin60^2+cos60^2";
+const matches = input.match(/\b(?:sqrt|sin|cos)\d+/g);
+
+if (matches) {
+  console.log(matches); // Output: ["sqrt4", "sin60", "cos60"]
+} else {
+  console.log("No expressions found.");
+}
+
+
+const input = "sqrt4/sin60^2-cos-60^2";
+const matches = input.match(/\b\w+\b/g);
+
+if (matches) {
+  const expressions = matches.filter(match => match !== "sqrt" && match !== "sin" && match !== "cos");
+  console.log(expressions); // Output: ["sqrt4", "sin60", "cos-60.0"]
+} else {
+  console.log("No expressions found.");
+}
+
+
+const matches = input.match(/[a-z]+\s*-?\d+(\.\d+)?/gi);
+
+
+you need this.
+
+
+    */
     
     scientific_operation(source){
         var resultvalue = new Decimal("0.0");
         var input = source;//sin60,sqrt4,log
         
-        input = input.replaceAll("--", "+");
-        input = input.replaceAll("+", " +");
-        input = input.replaceAll("-", " -");
-        input = input.replaceAll("/", " /");
-        input = input.replaceAll("*", " *");
-        input = input.replaceAll("^", " ^ ");
-        input = input.replaceAll("sin", " sin");
-        input = input.replaceAll("log", " log");
-        input = input.replaceAll("cos", " cos");
-        input = input.replaceAll("tan", " tan");
-        input = input.replaceAll("sqr", " sqr");
-        input = input.replaceAll("sin -", "sin-");
-        input = input.replaceAll("cos -", "cos-");
-        input = input.replaceAll("tan -", "tan-");
-        input = input.replaceAll("a sin", "asin");
-        input = input.replaceAll("a cos", "acos");
-        input = input.replaceAll("a tan", "atan");
-        input = input.replaceAll("(", " ( ");
-        input = input.replaceAll(")", " ) ");
+        // input = input.replaceAll("--", "+");
+        // input = input.replaceAll("+", " +");
+        // input = input.replaceAll("-", " -");
+        // input = input.replaceAll("/", " /");
+        // input = input.replaceAll("*", " *");
+        // input = input.replaceAll("^", " ^ ");
+        // input = input.replaceAll("sin", " sin");
+        // input = input.replaceAll("log", " log");
+        // input = input.replaceAll("cos", " cos");
+        // input = input.replaceAll("tan", " tan");
+        // input = input.replaceAll("sqr", " sqr");
+        // input = input.replaceAll("sin -", "sin-");
+        // input = input.replaceAll("cos -", "cos-");
+        // input = input.replaceAll("tan -", "tan-");
+        // input = input.replaceAll("a sin", "asin");
+        // input = input.replaceAll("a cos", "acos");
+        // input = input.replaceAll("a tan", "atan");
+        // input = input.replaceAll("(", " ( ");
+        // input = input.replaceAll(")", " ) ");
         
-        let elements = input.split(' ');
-        elements = elements.filter(item => item !== "");
+        // let elements = input.split(' ');
+        // elements = elements.filter(item => item !== "");
     
         //(elements)
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[i].includes("asin")) {
-                if (this.isFloat(elements[i].replaceAll("asin", ""))){
-                    elements[i] = elements[i].replaceAll("asin", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.asin(arg));
-                    elements[i] = resultvalue.toString();
+        //(cos60+sin60+8)
+        input = input.replaceAll('(','');
+        input = input.replaceAll(')','');
+        const elements = input.match(/[a-z]+\s*-?\d+(\.\d+)?/gi);
+        if (elements) {
+            for (let i = 0; i < elements.length; i++){
+                if (elements[i].includes("asin")) {
+                    if (this.isFloat(elements[i].replaceAll("asin", ""))){
+                        let exp = elements[i].replaceAll("asin", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.asin(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
                 }
-            }
-            
-            if (elements[i].includes("acos")) {
-                if (this.isFloat(elements[i].replaceAll("acos", ""))){
-                    elements[i] = elements[i].replaceAll("acos", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.acos(arg));
-                    elements[i] = resultvalue.toString();
+                if (elements[i].includes("acos")) {
+                    if (this.isFloat(elements[i].replaceAll("acos", ""))){
+                        let exp = elements[i].replaceAll("acos", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.acos(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
                 }
-            }
-            
-            if (elements[i].includes("atan")) {
-                if (this.isFloat(elements[i].replaceAll("atan", ""))){
-                    elements[i] = elements[i].replaceAll("atan", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.atan(arg));
-                    elements[i] = resultvalue.toString();
+                if (elements[i].includes("atan")) {
+                    if (this.isFloat(elements[i].replaceAll("atan", ""))){
+                        let exp = elements[i].replaceAll("atan", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.atan(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
                 }
-            }
-            
-            if (elements[i].includes("sin")) {
-                if (this.isFloat(elements[i].replaceAll("sin", ""))){
-                    elements[i] = elements[i].replaceAll("sin", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.sin(arg));
-                    elements[i] = resultvalue.toString();
+                if (elements[i].includes("sin")) {
+                    if (this.isFloat(elements[i].replaceAll("sin", ""))){
+                        let exp = elements[i].replaceAll("sin", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.sin(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
                 }
-            }
-           
-            if (elements[i].includes("cos")) {
-                if (this.isFloat(elements[i].replaceAll("cos", ""))){
-                    elements[i] = elements[i].replaceAll("cos", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.cos(arg));
-                    elements[i] = resultvalue.toString();
+                if (elements[i].includes("cos")) {
+                    if (this.isFloat(elements[i].replaceAll("cos", ""))){
+                        let exp = elements[i].replaceAll("cos", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.cos(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
                 }
-            }
-            
-            if (elements[i].includes("tan")) {
-                if (this.isFloat(elements[i].replaceAll("tan", ""))){
-                    elements[i] = elements[i].replaceAll("tan", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.tan(arg));
-                    elements[i] = resultvalue.toString();
+                if (elements[i].includes("tan")) {
+                    if (this.isFloat(elements[i].replaceAll("tan", ""))){
+                        let exp = elements[i].replaceAll("tan", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.tan(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
+                }   
+                if (elements[i].includes("sqrt")) {
+                    if (this.isFloat(elements[i].replaceAll("sqrt", ""))){
+                        let exp = elements[i].replaceAll("sqrt", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.sqrt(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
+                }    
+                if (elements[i].includes("abs")) {
+                    if (this.isFloat(elements[i].replaceAll("abs", ""))){
+                        let exp = elements[i].replaceAll("abs", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.abs(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
+                }  
+                if (elements[i].includes("exp")) {
+                    if (this.isFloat(elements[i].replaceAll("exp", ""))){
+                        let exp = elements[i].replaceAll("exp", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.exp(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
                 }
-            }
-               
-            if (elements[i].includes("sqrt")) {
-                if (this.isFloat(elements[i].replaceAll("sqrt", ""))){
-                    elements[i] = elements[i].replaceAll("sqrt", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.sqrt(arg));
-                    elements[i] = resultvalue.toString();
+                if (elements[i].includes("logd")) {
+                    if (this.isFloat(elements[i].replaceAll("logd", ""))){
+                        let exp = elements[i].replaceAll("logd", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.log10(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
+                } 
+                if (elements[i].includes("logb")) {
+                    if (this.isFloat(elements[i].replaceAll("logb", ""))){
+                        let exp = elements[i].replaceAll("logb", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.log2(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
                 }
-            }
-            
-            //http://swift.tecc0.com/?p=105
-            if (elements[i].includes("abs")) {
-                if (this.isFloat(elements[i].replaceAll("abs", ""))){
-                    elements[i] = elements[i].replaceAll("abs", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.abs(arg));
-                    elements[i] = resultvalue.toString();
+                if (elements[i].includes("log")) {
+                    if (this.isFloat(elements[i].replaceAll("log", ""))){
+                        let exp = elements[i].replaceAll("log", "");
+                        let arg = new Decimal(exp);
+                        let resultvalue = new Decimal(Math.log(arg));
+                        input = input.replaceAll(elements[i], resultvalue.toString());
+                    }
                 }
-            }
-            
-            if (elements[i].includes("exp")) {
-                if (this.isFloat(elements[i].replaceAll("exp", ""))){
-                    elements[i] = elements[i].replaceAll("exp", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.exp(arg));
-                    elements[i] = resultvalue.toString();
-                }
-            }
-            
-            if (elements[i].includes("logb")) {
-                if (this.isFloat(elements[i].replaceAll("logb", ""))){
-                    elements[i] = elements[i].replaceAll("logb", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.log2(arg));
-                    elements[i] = resultvalue.toString();
-                }
-            }
-            
-            if (elements[i].includes("logd")) {
-                if (this.isFloat(elements[i].replaceAll("logd", ""))){
-                    elements[i] = elements[i].replaceAll("logd", "");
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.log10(arg));
-                    elements[i] = resultvalue.toString();
-                }
-            }
-            
-            if (elements[i].includes("log")) {
-                if (this.isFloat(elements[i].replaceAll("log", ""))){
-                    elements[i] = elements[i].replaceAll('log', '');
-                    let arg = new Decimal(elements[i]);
-                    resultvalue = new Decimal(Math.log(arg));
-                    elements[i] = resultvalue.toString();
-                }
-            }
+            }   
         }
-        
-        return elements.join('');
+        return input;
     }
     
     isFloat(str) {
         return /^-?\d+(\.\d+)?$/.test(str);
     }  
 
+    areAllFloats(item) {
+        return !isNaN(parseFloat(item));
+      }
+
+    containsAlphabetChars(str) {
+        return /[a-zA-Z]/.test(str);
+    }
+
     basic_operation(source){
-        
         var resultvalue = new Decimal("0.0");
         var input = source;
-        
         input = input.replaceAll("--" , "+");
-        input = input.replaceAll("+" , " ");
+        input = input.replaceAll("+" , " + ");
         input = input.replaceAll("/" , " / ");
         input = input.replaceAll("-" , " -");
         input = input.replaceAll("*" , " * ");
         input = input.replaceAll("^" , " ^ ");
+
+        
         
         let elements = input.split(' ');
-        elements = elements.filter(item => item !== "");
-        for (let i = 0; i < elements.length; i++) {
-            if (elements[i] == "^" &&  typeof elements[i-1] !== "undefined" && this.isFloat(elements[i-1]) && typeof elements[i+1] !== "undefined" && this.isFloat(elements[i+1])) {
-                // if(this.isFloat(elements[i+1])){
-                    // elements[i+1] = "error";
-                    // elements[i-1] = "nil";
-                    // elements[i] = "nil";
-                    //error
-                // }
-                // else{                    
+        elements = elements.filter(function(item) {
+            return item !== "nil" && item !== "" && item !== null && item !== undefined;
+        });
+        
+        let checkings = Array.from(elements);
+        checkings = checkings.filter(function(item) {
+            return item !== "+";
+        });
+
+        if (checkings.every(this.areAllFloats)) {
+            elements = checkings;        
+            //MUST NEED
+            if (elements.length > 1) {
+                for (let i = 0; i < elements.length; i++){
+                    if (this.isFloat(elements[i-1]) && this.isFloat(elements[i])){
+                        let a = new Decimal(elements[i-1]);
+                        let b = new Decimal(elements[i]);
+                        let resultvalue = a.plus(b);
+                        elements[i] = resultvalue.toString();
+                        elements[i-1] = "nil";
+                    }
+                }
+            }
+            elements = elements.filter(function(item) {
+                return item !== "nil";
+            });
+            return elements.join('');
+        
+            
+        }else{
+            for (let i = 0; i < elements.length; i++) {
+                if (elements[i] == "^" &&  typeof elements[i-1] !== "undefined" && this.isFloat(elements[i-1]) && typeof elements[i+1] !== "undefined" && this.isFloat(elements[i+1])) {                    
                     let a = new Decimal(elements[i-1]);
                     let b = new Decimal(elements[i+1]);
-                    var c = Math.pow(a, b);
+                    var c =  a.pow(b);
                     elements[i+1] = c.toString();
                     elements[i-1] = "nil";
-                    elements[i] = "nil";
-                // }                
-            }
-            
-            if (elements[i] == "*" && typeof elements[i-1] !== "undefined" && this.isFloat(elements[i-1]) && typeof elements[i+1] !== "undefined" && this.isFloat(elements[i+1])) {
-                let a = new Decimal(elements[i-1])
-                let b = new Decimal(elements[i+1])
-                resultvalue = a.times(b); 
-                elements[i+1] = resultvalue.toString();
-                elements[i-1] = "nil";
-                elements[i] = "nil";
-            }
-            
-            if (elements[i] == "/" && this.isFloat(elements[i-1]) && this.isFloat(elements[i+1])){
-                let a = new Decimal(elements[i-1]);
-                let b = new Decimal(elements[i+1]);
-                if (elements[i+1] == "0") {
-                    elements[i-1] = "nil";
-                    elements[i] = "nil";
-                    elements[i+1] = "nil";
-                }
-                else{
-                    resultvalue = a.dividedBy(b);
-                    elements[i+1] = resultvalue.toString();
-                    elements[i-1] = "nil";
-                    elements[i] = "nil";
+                    elements[i] = "nil";                
                 }
             }
+            elements = elements.filter(function(item) {
+                return item !== "nil";
+            });
+
+            return elements.join('');
         }
-        
-        elements = elements.filter(function(item) {
-            return item !== "nil";
-        });
-          
-        //MUST NEED
-        if (elements.length > 1) {
-            for (let i = 0; i < elements.length; i++){
-                if (this.isFloat(elements[i-1]) && this.isFloat(elements[i])){
-                    let a = new Decimal(elements[i-1]);
-                    let b = new Decimal(elements[i]);
-                    let resultvalue = a.plus(b);
-                    elements[i] = resultvalue.toString();
-                    elements[i-1] = "nil";
-                }
-            }
-        }
-        elements = elements.filter(function(item) {
-            return item !== "nil";
-        });
-        return elements.join('');
+        // elements = elements.filter(item => item !== "");
     }
 }
 
