3f0475d5016f32a52a863f07025268bf58de076d
Yujin Yano <northwindapps@gmail.com>
2023-08-20 19:02:57 +0900

    diffrential equaton

--- __tests__/sample.test.js
+++ __tests__/sample.test.js
@@ -5,6 +5,18 @@ test('adds 1 + 2 to equal 3', () => {
   expect(parseFloat(service.excecute(`1+2`))).toBe(3);
 });
 
+test('adds (1 + 2) * (3 - 1) to equal 6', () => {
+  expect(parseFloat(service.excecute(`(1 + 2) * (3 - 1)`))).toBe(6);
+});
+
+test('adds (1 + 2) * (-1 + 3) to equal 6', () => {
+  expect(parseFloat(service.excecute(`(1 + 2) * (-1 + 3)`))).toBe(6);
+});
+
+test('adds (1 - 2) * (-1 + 3) to equal -2', () => {
+  expect(parseFloat(service.excecute(`(1 - 2) * (-1 + 3)`))).toBe(-2);
+});
+
 test('cos45 * sin45 to equal 0.5', () => {
   expect(parseFloat(service.excecute(`cos45 * sin45`))).toBeCloseTo(0.5,5);
 });
