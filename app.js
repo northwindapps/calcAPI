2f7c51f1d27efab77336bffd40817eabab1371d1
Yujin Yano <northwindapps@gmail.com>
2023-08-23 08:49:55 +0900

    making runge_kutta

--- app.js
+++ app.js
@@ -21,6 +21,33 @@ app.post('/api/v1/execute', (req, res) => {
     res.status(200).json({ result: result });
 });
 
+// GET route
+app.get('/api/v1/runge_kutta4', (req, res) => {
+    const fx = req.query.fx;
+    const x0 = req.query.x0;
+    const y0 = req.query.y0;
+    const h = req.query.h;
+    const l = req.query.l;
+
+    try {
+        let result2 = null;
+        if (l === null) {
+            result2 = service.runge_kutta_method(fx,x0,y0,h,null);
+        }else{
+            result2 = service.runge_kutta_method(fx,x0,y0,h);
+        }
+        const data = {
+            x: result2[0],
+            y: result2[1]
+        };
+        res.status(200).json(data);
+      } catch (error) {
+        // Handle errors here
+        console.log(error);
+        res.status(500).json({ error: 'An error occurred' });
+      }
+});
+
 // Start the server
 app.listen(port, () => {
     console.log(`Server is listening on port ${port}`);
